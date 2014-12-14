43b98fb14fd2fe98bd118e27be98b6580984c5a2
Jason Blalock <jasonblalock@users.noreply.github.com>
2014-12-14 02:09:39 -0500

    Update scripts to move user permission level setup to non-sudo script

--- part1.sh
+++ part1.sh
@@ -6,9 +6,7 @@ rubyinstallversion=0.5.0
 
 getinfo()
 {
-  read -p "Enter your name for git: " gitname
-  read -p "Enter your username for github : " gitusername
-  read -p "Enter the ip address for your server (192.168.1.22): " staticip
+  read -p "Enter the ip address for your server (192.168.33.10): " staticip
   # read -p "Enter the netmask for your network: (looks like 255.255.255) " netmask
 }
 
@@ -16,20 +14,18 @@ writeinterfacefile()
 {
   cat << EOF >> /etc/network/interfaces
 
-#Your static network configuration
-auto eth1
-iface eth1 inet static
-address $staticip
-netmask 255.255.255.0
-EOF
+  #Your static network configuration
+  auto eth1
+  iface eth1 inet static
+  address $staticip
+  netmask 255.255.255.0
+  EOF
 }
 
 confirmation()
 {
   sudo -u $1 echo ""
   sudo -u $1 echo "So your settings are:"
-  sudo -u $1 echo "Your git config name is: " $gitname
-  sudo -u $1 echo "Your Github username is: " $gitusername
   sudo -u $1 echo "Your IP is:              " $staticip
   # sudo -u $1 echo "You subnet mask is " $netmask
   sudo -u $1 echo ""
@@ -56,11 +52,6 @@ aptitude -y safe-upgrade
 aptitude -y install build-essential vim ruby-dev git libsqlite3-dev openssh-server
 vmware-config-tools.pl -d
 
-sudo -u $1 git config --global user.name $gitname
-sudo -u $1 git config --global user.email "${gitusername}@users.noreply.github.com"
-sudo -u $1 git config --global core.autocrlf input
-sudo -u $1 git config --global branch.autosetuprebase always
-
 sudo -u $1 wget -O "chruby-${chrubyversion}.tar.gz" "https://github.com/postmodern/chruby/archive/v${chrubyversion}.tar.gz"
 sudo -u $1 tar -xzvf "chruby-$chrubyversion.tar.gz"
 cd "chruby-${chrubyversion}/"
@@ -82,8 +73,6 @@ cd "ruby-install-${rubyinstallversion}/"
 make install
 cd ~
 sudo -u $1 ruby-install ruby $rubyversion
-sudo -u $1 echo "gem: --no-document" > ~/.gemrc
-sudo -u $1 echo "ruby-${rubyversion}" > ~/.ruby-version
 
 read -p "Restart? [y/N] " yn
 case $yn in
--- part2.sh
+++ part2.sh
@@ -1,5 +1,41 @@
 #!/bin/bash
 
+getinfo()
+{
+  read -p "Enter your name for git: " gitname
+  read -p "Enter your username for github : " gitusername
+}
+
+confirmation()
+{
+  echo ""
+  echo "So your settings are:"
+  echo "Your git config name is: " $gitname
+  echo "Your Github username is: " $gitusername
+  echo ""
+
+  while true; do
+    read -p "Are these informations correct? [y/N] " yn
+    case $yn in
+      [Yy]* ) break;;
+      [Nn]* ) getinfo ;;
+      * ) echo "Please enter Y or n ";;
+    esac
+  done
+}
+
+cd ~
+getinfo
+confirmation
+
+git config --global user.name $gitname
+git config --global user.email "${gitusername}@users.noreply.github.com"
+git config --global core.autocrlf input
+git config --global branch.autosetuprebase always
+
+echo "gem: --no-document" > ~/.gemrc
+echo "ruby-${rubyversion}" > ~/.ruby-version
+
 cd ~
 gem update --system
 gem install bundler
@@ -7,7 +43,6 @@ gem install rake
 gem install berkshelf
 
 sudo chruby-exec -- gem install chef
-gem install berkshelf
 wget https://github.com/jasonblalock/rails-dev-box/archive/master.tar.gz
 tar -xzvf master.tar.gz
 cd rails-dev-box-master
