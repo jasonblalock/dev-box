911d0aa262a4c54b21f4a57d27483ff20c6432a6
Jason Blalock <jasonblalock@users.noreply.github.com>
2015-07-07 16:35:31 -0400

    iojs install not working: removed. Just install nvm.

--- kitchen/site-cookbooks/nvm/recipes/iojs.rb
+++ kitchen/site-cookbooks/nvm/recipes/iojs.rb
@@ -1,10 +1,9 @@
 include_recipe 'nvm'
 
-version = 'iojs'
-node.set['nvm']['nvm_install_test']['version'] = version
+# currently not working. Need to investigate
 
-nvm_install version  do
-  from_source false
-  alias_as_default true
-  action :create
-end
+# nvm_install 'iojs'  do
+#   from_source false
+#   alias_as_default true
+#   action :create
+# end
